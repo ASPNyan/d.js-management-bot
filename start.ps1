8d17c93103938ac99fd3d7ee5afd6ff4dbd0f295
Nyan <85216339+ASPNyan@users.noreply.github.com>
2022-06-16 15:07:22 +0930

    Add start ps script

---
+++ start.ps1
@@ -0,0 +1,10 @@
+﻿$Current = Test-Path -Path .\setup.ps1
+$Containing = Test-Path -Path ..\setup.ps1
+if ($Current = $True) {
+  Remove-Item -Path .\setup.ps1
+}
+if ($Containing = $True) {
+  Remove-Item -Path ..\setup.ps1
+}
+
+npm run dev
