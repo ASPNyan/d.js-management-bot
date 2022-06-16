996cd77da2c2d7aee4e770278dc77b6c69507019
Nyan <85216339+ASPNyan@users.noreply.github.com>
2022-06-16 14:44:01 +0930

    Create setup ps script (testing atm)

---
+++ setup.ps1
@@ -0,0 +1,12 @@
+Invoke-WebRequest 'https://github.com/ASPNyan/d.js-management-bot/archive/refs/heads/main.zip' -OutFile .\management-bot.zip
+Expand-Archive .\management-bot.zip .\
+Rename-Item .\d.js-management-bot-main .\Management-Bot
+Remove-Item .\management-bot.zip
+cd .\Management-Bot
+Remove-Item .\.env
+
+$Token = Read-Host -Prompt 'Input Your Bot Token Here'
+$Mongo = Read-Host -Prompt 'Input Your MongoDB URI Here'
+
+Add-Content -Path .\.env -Value 'TOKEN='$Token
+Add-Content -Path .\.env -Value 'MONGODB='$Mongo
