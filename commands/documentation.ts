8a359bc5b63ef0c07dcf13ce9b585777438cf2d1
Nyan <85216339+ASPNyan@users.noreply.github.com>
2022-06-28 11:41:11 +0930

    Update documentation.ts to add Hyperlinks (and fix prev links)

--- commands/documentation.ts
+++ commands/documentation.ts
@@ -12,25 +12,25 @@ export default {
         const gitlink = 'https://github.com/ASPNyan/d.js-management-bot'
         const embed = new MessageEmbed()
         .setTitle(`Command Documentation`)
-        .setDescription(`${gitlink}#command-documentation`)
-        .setURL(`${gitlink}`)
+        .setDescription(`You can Find the GitHub Repo [**Here**](${gitlink})`)
+        .setURL(`${gitlink}#command-documentation`)
         .setColor(`GREEN`)
         .setAuthor({name: `ASPNyan`, iconURL: `https://avatars.githubusercontent.com/u/85216339?v=4`, url: `https://github.com/ASPNyan`})
         .addFields([
             {
-                name: `__User Management__ (${gitlink}#user-management)`,
-                value: `**Ban & Kick** (${gitlink}#ban--kick)\n\n**Roles** (${gitlink}#roles)\n\n**Timeout** (${gitlink}#timeout)\n\nWarn Add, Remove, and List (${gitlink}#warn-add-remove-and-list)`
+                name: `[__User Management__](${gitlink}#user-management)`,
+                value: `[**Ban & Kick**](${gitlink}#ban--kick)\n\n[**Roles**](${gitlink}#roles)\n\n[**Timeout**](${gitlink}#timeout)\n\n[**Warn Add, Remove, and List**](${gitlink}#warn-add-remove-and-list)`
             },
             {
-                name: `__Text & Voice Administration__ (${gitlink}#text--voice-administration)`,
-                value: `**VCMute, VCDeafen & VCDisconnect** (${gitlink}#vcmute-vcdeafen--vcdisconnect)\n\n**Purge** (${gitlink}#purge)`
+                name: `[__Text & Voice Administration__](${gitlink}#text--voice-administration)`,
+                value: `[**VCMute, VCDeafen & VCDisconnect**](${gitlink}#vcmute-vcdeafen--vcdisconnect)\n\n[**Purge**](${gitlink}#purge)`
             },
             {
-                name: `__Configuration__`,
-                value: `**Send & RolesMenu** (${gitlink}#send--rolesmenu)`
+                name: `[__Configuration__](${gitlink}#configuration)`,
+                value: `[**Send & RolesMenu**](${gitlink}#send--rolesmenu)`
             }
         ])
-        .setFooter('The framework for this bot was made by ASPNyan on Github ツ')
+        .setFooter(`The framework for this bot was made by [ASPNyan](https://github.com/ASPNyan) on Github ツ`)
         
         interaction.reply({
             embeds: [embed],
